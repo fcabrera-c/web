4b1f425e819bacc2e3a2828ffe2dd17acafba56b
fcabrera-c <75145601+fcabrera-c@users.noreply.github.com>
2021-01-05 12:35:22 -0600

    Update Home-steps.js

--- webcorporativa/src/test/resources/steps/Home-steps.js
+++ webcorporativa/src/test/resources/steps/Home-steps.js
@@ -9,7 +9,6 @@ Given(/^we validate the first section of the page$/, function() {
   expect($(".bold")).to.exist();
   expect($(".active")).to.exist();
   expect($(".theme-dark")).to.exist();
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion1.png');
 });
 
 Given(/^we validate the second section of the page$/, function() {
@@ -19,7 +18,6 @@ Given(/^we validate the second section of the page$/, function() {
   expect($("body > app-root > div > div.body > ad-home > ad-how-we-do-it > div > div > div.subtitle.ad-prod-body-1")).to.have.text("Soluciones estratégicas a la medida que proyectan tu negocio a los retos del futuro.");
   expect($("body > app-root > div > div.body > ad-home > ad-how-we-do-it > div > div > div.circles")).to.exist();
   $(":root").waitTime(2, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion2.png');
 });
 
 Given(/^we validate the third section of the page$/, function() {
@@ -33,7 +31,6 @@ Given(/^we validate the third section of the page$/, function() {
   expect($(".tertiary-70")).to.exist();
   var timeUnits = require("minium/timeunits");
   $(":root").waitTime(2, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion3.png');
 });
 Given(/^we validate the fourth section of the page$/, function() {
   $("body > app-root > div > div.body > ad-home > ad-solutions > div").scrollIntoView();
@@ -41,7 +38,6 @@ Given(/^we validate the fourth section of the page$/, function() {
   expect($("body > app-root > div > div.body > ad-home > ad-solutions > div > div > div.solutions-content.col > div > div.title.ad-prod-h3.bold")).to.have.text("Soluciones globales");
   expect($("body > app-root > div > div.body > ad-home > ad-solutions > div > div > div.solutions-content.col > div > div.subtitle.ad-prod-subtitle-1")).to.exist();
   $(":root").waitTime(2, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion4.png');
 });
 
 Given(/^we validate the fifth section of the page$/, function() {
@@ -56,7 +52,6 @@ Given(/^we validate the fifth section of the page$/, function() {
   expect($(".red-to-purple-medium")).to.have.text("Wallet solution");
   var timeUnits = require("minium/timeunits");
   $(":root").waitTime(2, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion5.png');
 });
 
 Given(/^we validate the sexth section of the page$/, function() {
@@ -66,27 +61,22 @@ Given(/^we validate the sexth section of the page$/, function() {
   expect($("#confianza > div > div.row.clients-logos > div:nth-child(1)")).to.exist("Citibanamex");
   $("#confianza > div > div.row.clients-logos > div:nth-child(1)").moveTo();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion6-1.png');
   $(":root").waitTime(1, timeUnits.SECONDS);
   expect($("#confianza > div > div.row.clients-logos > div:nth-child(2)")).to.exist("Scotiabanck");
   $("#confianza > div > div.row.clients-logos > div:nth-child(2)").moveTo();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion6-2.png');
   $(":root").waitTime(1, timeUnits.SECONDS);
   expect($("#confianza > div > div.row.clients-logos > div:nth-child(3)")).to.exist("Santander");
   $("#confianza > div > div.row.clients-logos > div:nth-child(3)").moveTo();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion6-3.png');
   $(":root").waitTime(1, timeUnits.SECONDS);
   expect($("#confianza > div > div.row.clients-logos > div:nth-child(4)")).to.exist("BBVA");
   $("#confianza > div > div.row.clients-logos > div:nth-child(4)").moveTo();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion6-4.png');
   $(":root").waitTime(1, timeUnits.SECONDS);
   expect($("#confianza > div > div.row.clients-logos > div:nth-child(5)")).to.exist("Banorte");
   $("#confianza > div > div.row.clients-logos > div:nth-child(5)").moveTo();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion6-5.png');
   $(":root").waitTime(1, timeUnits.SECONDS);
 });
 
@@ -98,7 +88,6 @@ Given(/^we validate the seventh section of the page$/, function() {
   expect($("#colabora > div > div > div > a")).to.have.text("Ver todas las vacantes");
   var timeUnits = require("minium/timeunits");
   $(":root").waitTime(2, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion7.png');
 });
 
 Given(/^we validate the eigth section of the page$/, function() {
@@ -108,7 +97,6 @@ Given(/^we validate the eigth section of the page$/, function() {
   expect($("#idea > div > div > div > a > an-icon")).to.exist("Cuéntanos sobre ella");
   var timeUnits = require("minium/timeunits");
   $(":root").waitTime(2, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/seccion8.png');
 });
 
 //Given(/^we validate all the links of each section$/, function() {
@@ -142,16 +130,12 @@ Given(/^we validate the footer of page "([^"]*)"$/, function(arg1) {
   $("#main-footer > div > div > div > figure > img").scrollIntoView();
   $("#main-footer > div > div > div > section > a.menu-social.footer-facebook > an-icon").moveTo();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/footerfb9.png');
   $("#main-footer > div > div > div > section > a.menu-social.footer-twitter").moveTo();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/footertw10.png');
   $("#main-footer > div > div > div > section > a.menu-social.footer-linkedin").moveTo();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/footerldn11.png');
   $("#main-footer > div > div > div > section > a.menu-social.footer-instagram").moveTo();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo('C:/Users/Anzen/Documents/Minium/Evidencias Web/Inicio/footerinst12.png');
 //  $("#main-footer > div > div > div > section > a.menu-social.footer-facebook").click();
 //  $("#main-footer > div > div > div > section > a.menu-social.footer-twitter").click();
 //  $("#main-footer > div > div > div > section > a.menu-social.footer-linkedin").click();
