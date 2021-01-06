1609e28b733bedbdb9246acf9932766a0d6f9ce6
fcabrera-c <75145601+fcabrera-c@users.noreply.github.com>
2021-01-05 19:15:46 -0600

    Update Home-steps.js

--- webcorporativa/src/test/resources/steps/Home-steps.js
+++ webcorporativa/src/test/resources/steps/Home-steps.js
@@ -150,7 +150,5 @@ Given(/^we validate the footer of page "([^"]*)"$/, function(arg1) {
   $(":root").waitTime(2, timeUnits.SECONDS);
   scenario.embed(browser.screenshot().asBytes(), "image/png");
   $("#main-footer > div > div > div > section > a.menu-social.footer-instagram").moveTo();
-  $(":root").waitTime(1, timeUnits.SECONDS);
-  scenario.embed(browser.screenshot().asBytes(), "image/png");
-  $(":root").waitTime(3, timeUnits.SECONDS);
+  $(":root").waitTime(2, timeUnits.SECONDS);
 });
